f12eae45072d09e2f768bb12c440240abddd58ee
olivamat <matiasholeh@gmail.com>
2019-06-05 20:42:14 +0300

    Updates

---
+++ precache-manifest.71d64f111fed8c9953b855ce31b8d48b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfa5ca2ebccd539feaf0cde7bb0e31d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b55a689fc52cf91a8f2",
+    "url": "/static/css/main.cd108a4b.chunk.css"
+  },
+  {
+    "revision": "0fe4e9530863142842cf",
+    "url": "/static/js/2.1e4c2935.chunk.js"
+  },
+  {
+    "revision": "5b55a689fc52cf91a8f2",
+    "url": "/static/js/main.b6f34395.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
